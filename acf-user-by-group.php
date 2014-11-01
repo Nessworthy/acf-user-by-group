fda934e8dcd30cedbf80c40d936e25e693dd2ec4
Sean Nessworthy <bladescope@googlemail.com>
2014-11-01 14:38:06 +0000

    The first step is always the hardest!

---
+++ acf-user-by-group.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: ACF User by Group
+ * Plugin URI: https://github.com/Nessworthy/acf-user-by-group
+ * Description: Allows admins to filter Advanced Custom Field's field groups by a user's group from the Groups plugin.
+ * Version: 1.0
+ * Author: Sean Nessworthy
+ * Author URI: http://nessworthy.me
+ * License: GPL2
+ */
+
+
+// No meddling kids.
+defined('ABSPATH') or die();
+
+
+
+?>
